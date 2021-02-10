f09df1080f02a863fd63f80d3409aec743ff5d59
Choiyunh_ <svs101@naver.com>
2021-02-10 22:00:57 +0900

    Feature: #2 - 원 검출 조건 상세

--- TrafficLight.py
+++ TrafficLight.py
@@ -3,20 +3,30 @@ import cv2
 
 image = cv2.imread('./img/b_red.png')
 cv2.imshow("original", image)
-image = image[220:280, 250:380]
+image = image[150:350, 250:380]
 
 image2 = image.copy()
 image2 = cv2.GaussianBlur(image2, (9, 9), 0)
 imgray = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
 
-circles = cv2.HoughCircles(imgray, cv2.HOUGH_GRADIENT, 1, 10, param1=60, param2=30, minRadius=0, maxRadius=75)
-
+circles = cv2.HoughCircles(imgray, cv2.HOUGH_GRADIENT, 1, 10, param1=60, param2=30, minRadius=0, maxRadius=30)
+cv2.imshow("imgray", imgray)
 if circles is not None:
     circles = np.uint16(np.around(circles))
     print(circles)
-    print(len(circles[0]))
-    for i in circles[0, :]:
-        cv2.circle(image, (i[0], i[1]), i[2], (255, 0, 0), 2)
+    for circle in circles[0, :]:
+        cv2.circle(image, (circle[0], circle[1]), circle[2], (255, 0, 0), 2)
+    # print(imgray[54, 98])  # 녹색 신호 자리
+    # print(imgray[54, 68])  # 황색 신호 자리
+    # print(imgray[54, 36])  # 적색 신호 자리
+
+    # print(imgray[circles[0][0][1], circles[0][0][0]])
+
+    if len(circles[0]) == 3:
+        if circles[0][0][1] < 60 and circles[0][0][0] < 200:
+            print('a')
+        for circle in circles[0, :]:
+            print(circle[0])
 
     cv2.imshow('HoughCircle', image)
 else:
@@ -29,7 +39,7 @@ red_range = cv2.inRange(image, lower_red, upper_red)
 
 red_result = cv2.bitwise_and(image, image, mask=red_range)
 
-#cv2.imshow("red", red_result)
+# cv2.imshow("red", red_result)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
