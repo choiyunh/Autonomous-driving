deabd5710eb7b2af5db4314b9eca0fa8c84e6264
Choiyunh_ <svs101@naver.com>
2021-02-15 18:46:06 +0900

    Feature: #5 - 미션 주행

--- auto_drive.py
+++ auto_drive.py
@@ -14,7 +14,7 @@ import cv2, random, math, time
 from cv_bridge import CvBridge
 from xycar_motor.msg import xycar_motor
 from sensor_msgs.msg import Image, LaserScan
-from detect_stopline import detect_stopline
+from detect_stopline import detect_stoplineB
 
 import sys
 import os
@@ -37,6 +37,7 @@ Height = 480
 Offset = 400
 Gap = 40
 flag = 0
+stop_flag = 0
 limit = 0
 
 
@@ -55,14 +56,14 @@ def scan_front(degree_s, degree_e, dist):
 
     for degree in range(degree_s, degree_e):  # scan degree
         if lidar_range[degree] <= dist:
-            print
-            lidar_range[degree]
+            print(lidar_range[degree])
             return False
     return True
 
 
 # publish xycar_motor msg
 def drive(Angle, Speed):
+    print(Angle, Speed)
     global pub
     msg = xycar_motor()
     msg.angle = Angle
@@ -240,57 +241,62 @@ def process_image(frame):
 def light_detection(frame):
     global limit
     frame = frame[150:250, 180:480]
-    cv2.imshow('frame', frame)
+
     frame2 = frame.copy()
     frame2 = cv2.GaussianBlur(frame2, (9, 9), 0)
     imgray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
-    circles = cv2.HoughCircles(imgray, cv2.HOUGH_GRADIENT, 1, 10, param1=60, param2=25, minRadius=5, maxRadius=20)
+    circles = cv2.HoughCircles(imgray, cv2.HOUGH_GRADIENT, 1, 10, param1=60, param2=25, minRadius=10, maxRadius=40)
 
     if circles is not None:
         circles = np.uint16(np.around(circles))
-        print(circles)
+
         for circle in circles[0, :]:
             cv2.circle(frame, (circle[0], circle[1]), circle[2], (255, 0, 0), 2)
+            cv2.imshow('frame', frame)
+    if len(circles[0]) == 3:
+        lightX = [circles[0][0][0], circles[0][1][0], circles[0][2][0]]
+        lightY = [circles[0][0][1], circles[0][1][1], circles[0][2][1]]
+        lightX.sort()
+        lightY.sort()
 
-        if len(circles[0]) == 3:
-            lightX = [circles[0][0][0], circles[0][1][0], circles[0][2][0]]
-            lightX.sort()
+        if (lightX[0] + lightX[1] + lightX[2] + 2) // 3 == lightX[1]:
+            print('COLOR VALUE : ', imgray[lightY[2], lightX[2]])
+            if imgray[lightY[2], lightX[2]] > 230:  # light on
+                return True
+            return False
+    elif len(circles[0]) == 4:
+        lightX = [circles[0][0][0], circles[0][1][0], circles[0][2][0], circles[0][3][0]]
+        lightY = [circles[0][0][1], circles[0][1][1], circles[0][2][1], circles[0][3][1]]
+        lightX.sort()
+        lightY.sort()
 
-            if (lightX[0] + lightX[1] + lightX[2] + 2) // 3 == lightX[1]:
-                if imgray[circles[0][2][1], circle[0][2][0]] <= 160:  # light on
-                    return True
-                return False
-        elif len(circles[0]) == 4:
-            lightX = [circles[0][0][0], circles[0][1][0], circles[0][2][0]]
-            lightX.sort()
-
-            if (lightX[0] + lightX[1] + lightX[2] + 2) // 3 == lightX[1]:
-                if imgray[circles[0][0][1], circle[0][0][0]] <= 160:  # left light
-                    limit = 5
-                if imgray[circles[0][3][1], circle[0][3][0]] <= 160:  # right light
-                    limit = 6
-                return False
-
-    else:
-        return True
+        if (lightX[0] + lightX[3]) == (lightX[1] + lightX[2]):
+            if imgray[lightY[0], lightX[0]] >= 230:  # left light
+                limit = 5
+            elif imgray[lightY[3], lightX[3]] >= 230:  # right light
+                limit = 6
+            return False
 
 
-def avoid_obstacles(degree_s, degree_e):
+return True
+
+
+def avoid_obstacles():
     global limit, lidar_range, flag  # left = 0', right = 180'
 
-    for degree in range(degree_s, degree_e):
-        # Obstacles in front of vehicle -> go left
+    for degree in range(44, 190):
+        # Obstacles in front of vehicle -> turn left
         if flag == 0 and 44 < degree < 134 and lidar_range[degree] <= 0.75:
             flag = 1
             limit = 1
-            print('Go Left')
+            print('Turn Left')
             print(lidar_range[degree], degree)
 
         if flag == 1 and 175 < degree < 189 and 0.45 < lidar_range[degree] < 0.8:
             flag = 2
             limit = 2
-            print('Go Right')
+            print('Turn Right')
             print(lidar_range[degree], degree)
 
         if flag == 2 and 185 < degree < 189 and 0.1 < lidar_range[degree] < 0.3:
@@ -310,14 +316,14 @@ def avoid_obstacles(degree_s, degree_e):
 def start():
     global pub
     global image
-    global cap
+    global Gap
     global Width, Height
 
     pid_P = 0.7
     pid_I = 0
     pid_D = 0
     sum_angle = 0
-    mission = 0
+    mission = 3
 
     prev_angle = 0
 
@@ -326,11 +332,11 @@ def start():
     image_sub = rospy.Subscriber("/usb_cam/image_raw", Image, img_callback)
     lidar_sub = rospy.Subscriber("/scan", LaserScan, lidar_callback, queue_size=1)
 
-    print
-    "---------- Xycar A2 v1.1.0 ----------"
-    rospy.sleep(2)
+    print("---------- Xycar A2 v1.1.0 ----------")
+    rospy.sleep(3)
 
     mission_start = time.time()
+    print('START!!!!')
     while True:
         while not image.size == (640 * 480 * 3):
             continue
@@ -347,57 +353,62 @@ def start():
         # prev_angle = angle
         # drive(angle, 12)
         print('Mission', mission)
+        # drive(c, 3)
 
         if mission == 0:
-            if scan_front(44, 134, 0.4) and light_detection(image):
-                drive(0, 3)
+            # if scan_front(50, 130, 0.4) and light_detection(image):
+            if not light_detection(image) and detect_stoplineB(image):
+                drive(0, 0)  # detect stop line, not green light
+                rospy.sleep(1)
             else:
-                drive(0, 0)
-                if time.time() - mission_start >= 10:
+                # if light_detection(image):
+                #    drive(c, 6)
+                drive(c, 6)
+                if time.time() - mission_start >= 30:
                     mission = 1
                     mission_start = time.time()
-        if mission == 1:
-            if not detect_stopline(image):
-                drive(0, 3)
+        elif mission == 1:
+            if not detect_stoplineB(image):
+                drive(c, 3)
             else:
                 drive(0, 0)
                 if time.time() - mission_start >= 10:
                     mission = 2
                     mission_start = time.time()
-        if mission == 2:
+        elif mission == 2:
             if scan_front(0, 134, 0.6):
-                drive(0, 3)
+                drive(c, 3)
             else:
                 drive(0, 0)
-            if detect_stopline(image):
+            if detect_stoplineB(image):
                 mission = 3
-        if mission == 3:
-            avoid_obstacles(40, 190)
+        elif mission == 3:
+            avoid_obstacles()
             if limit == 0:
-                drive(0, 3)
-            if limit == 1:
+                drive(c, 3)
+            elif limit == 1:
                 drive(-40, 2)
-            if limit == 2:
+            elif limit == 2:
                 drive(50, 2)
-            if limit == 3:
+            elif limit == 3:
                 drive(20, 2)
-            if limit == 4:
+            elif limit == 4:
                 drive(-7, 2)
             if not light_detection(image):
                 drive(0, 0)
                 mission = 4
-        if mission == 4:
+        elif mission == 4:
             if not light_detection(image) and limit < 5:
                 drive(0, 0)
             if limit == 5:
                 drive(-40, 3)  # turn left
                 mission = 5
-            if limit == 6:
+            elif limit == 6:
                 drive(40, 3)  # turn right
                 mission = 5
-        if mission == 5:
-            if not detect_stopline(image):
-                drive(0, 3)
+        elif mission == 5:
+            if not detect_stoplineB(image):
+                drive(c, 3)
             else:
                 drive(0, 0)
                 return
@@ -416,6 +427,3 @@ def start():
 
 if __name__ == '__main__':
     start()
-
-
-
